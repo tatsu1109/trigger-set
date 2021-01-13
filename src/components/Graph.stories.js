35b30f6cf0056408401f10f878d27dc9d945f5b9
tatsu1109 <ts11252tt1109@gmail.com>
2021-01-13 20:37:56 +0900

    データ追加

--- src/components/Graph.stories.js
+++ src/components/Graph.stories.js
@@ -12,22 +12,22 @@ export const Default = Template.bind({});
 Default.args = {
   data: [
     {
-      subject: 'Math', A: 120, B: 110, fullMark: 150,
+      subject: 'Math', A: 120, B: 110, value: 150,
     },
     {
-      subject: 'Chinese', A: 98, B: 130, fullMark: 150,
+      subject: 'Chinese', A: 98, B: 130, value: 150,
     },
     {
-      subject: 'English', A: 86, B: 130, fullMark: 150,
+      subject: 'English', A: 86, B: 130, value: 150,
     },
     {
-      subject: 'Geography', A: 99, B: 100, fullMark: 150,
+      subject: 'Geography', A: 99, B: 100, value: 150,
     },
     {
-      subject: 'Physics', A: 85, B: 90, fullMark: 150,
+      subject: 'Physics', A: 85, B: 90, value: 150,
     },
     {
-      subject: 'History', A: 65, B: 85, fullMark: 150,
+      subject: 'History', A: 65, B: 85, value: 150,
     },
   ]
 };
