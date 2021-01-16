6709682cebb6d33d8f1710dad78255a066fc76c5
tatsu1109 <ts11252tt1109@gmail.com>
2021-01-17 02:37:05 +0900

    AppSync呼び出し実装

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import { Provider } from 'react-redux';
 import store from './lib/redux';
+import { listTriggerMasters } from './graphql/queries'
 
 import InboxScreen from './components/InboxScreen';
-
+import awsExports from "./aws-exports";
 import './index.css';
-function App() {
+
+Amplify.configure(awsExports);
+const App = () => {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+  
+  async function fetchTodos() {
+    try {
+      const todoData = await API.graphql(graphqlOperation(listTriggerMasters))
+      const todos = todoData.data
+      setData(todos)
+    } catch (err) { console.log('error fetching todos') }
+  }
+  
   return (
     <Provider store={store}>
       <InboxScreen />
     </Provider>
   );
 }
+
+
 export default App;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTriggerMaster = /* GraphQL */ `
+  subscription OnCreateTriggerMaster(
+    $key: Int
+    $name: String
+    $category: String
+  ) {
+    onCreateTriggerMaster(key: $key, name: $name, category: $category) {
+      key
+      name
+      category
+    }
+  }
+`;
+export const onUpdateTriggerMaster = /* GraphQL */ `
+  subscription OnUpdateTriggerMaster(
+    $key: Int
+    $name: String
+    $category: String
+  ) {
+    onUpdateTriggerMaster(key: $key, name: $name, category: $category) {
+      key
+      name
+      category
+    }
+  }
+`;
+export const onDeleteTriggerMaster = /* GraphQL */ `
+  subscription OnDeleteTriggerMaster(
+    $key: Int
+    $name: String
+    $category: String
+  ) {
+    onDeleteTriggerMaster(key: $key, name: $name, category: $category) {
+      key
+      name
+      category
+    }
+  }
+`;
